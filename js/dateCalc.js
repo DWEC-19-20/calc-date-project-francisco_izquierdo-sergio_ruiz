beace85e7adb45e94ae8503080f161f1fc056093
PacoIzquierdo <55537076+PacoIzquierdo@users.noreply.github.com>
2019-10-21 10:04:26 +0200

    Add files via upload

--- js/dateCalc.js
+++ js/dateCalc.js
@@ -1,4 +1,4 @@
-
+const ms = 24 * 60 * 60 * 1000;
 
 /* Función que suma o resta un número de dias naturales según el valor de operation 
    startdate: objeto Fecha 
@@ -6,7 +6,12 @@
    return el resultado como un string en formato dd/mm/YYYY
 */
 function calcDate(startdate, days) { 
-  return new Date().toLocaleDateString("es-ES");
+    
+    startdate.setMonth(startdate.getMonth() - 1);
+    startdate.setDate(startdate.getDate() + days);
+    
+    return startdate.toLocaleDateString('es-ES');
+    
 }
 
 /* Función que recibe dos fechas de tipo Date y devuelva el el número de días naturales que hay entre
@@ -16,7 +21,17 @@ function calcDate(startdate, days) {
   return número de días naturales entre las dos fechas
 */
 function getDays(startdate, endDate) {
-   return 0;
+     
+    startdate.setMonth(startdate.getMonth() - 1);
+    endDate.setMonth(endDate.getMonth() - 1);
+    
+    var diaI = startdate.getTime();
+    var diaF = endDate.getTime();
+    var dif = diaF - diaI;
+    var FechaF = parseInt((dif/ms).toFixed(0));
+    
+    return FechaF;
+    
 }
 
 /* Función que suma o resta un número de dias hábiles según el valor de operation 
@@ -24,8 +39,38 @@ function getDays(startdate, endDate) {
    days: número de días hábiles
    return el resultado como un string en formato dd/mm/YYYY
 */
-function calcWorkingDate(startdate, days) { 
-  return new Date().toLocaleDateString("es-ES");
+function calcWorkingDate(startdate, days) {     
+    
+    startdate.setMonth(startdate.getMonth() - 1);
+    
+    var festivos = [["1Ene", "6Ene"],[],[],[],["1May"],[],[],["15Ag"],[],["12Oct"],["1Nov"],["6Dic", "8Dic", "25Dic"]];
+    
+    if(days > 0){
+        
+        for(let c = 0 ; c < days ; c++){
+        
+            startdate.setDate(startdate.getDate() + 1);
+        
+            if(startdate.getDay() == 0 || startdate.getDay() == 6) c--;
+          
+        }
+        
+    }
+    
+    else if(days < 0){
+        
+        for(let c = days ; c <= 0 ; c++){
+        
+            startdate.setDate(startdate.getDate() - 1);
+        
+            if(startdate.getDay() == 0 || startdate.getDay() == 6) c--;
+          
+        }
+        
+    }
+    
+    return startdate.toLocaleDateString('es-ES');
+    
 }
 
 /* Función que recibe dos fechas de tipo Date y devuelva el el número de días hábiles que hay entre
@@ -34,6 +79,6 @@ function calcWorkingDate(startdate, days) {
   endDate: objeto Fecha inicio
   return número de días hábiles entre las dos fechas*/
 function getWorkingDays(startdate, endDate) {
-   return 0;
+   return getDays(startdate, endDate);
 }
 
--- test/js/test.workingDays.js
+++ test/js/test.workingDays.js
@@ -6,15 +6,24 @@ suite("Testing calcDate", function() {
         expect(calcWorkingDate(new Date(2019, 10, 13),14)).to.equal("31/10/2019");
     });
     test("Test pasado", function() {
-        expect(calcWorkingDate(new Date(2019, 10, 31),-14)).to.equal("13/10/2019");
+        expect(calcWorkingDate(new Date(2019, 10, 13),1000)).to.equal("11/8/2023");
     });
+    test("Test pasado", function() {
+        expect(calcWorkingDate(new Date(2019, 10, 13),0)).to.equal("13/10/2019");
+    });
+    test("Test pasado", function() {
+        expect(calcWorkingDate(new Date(2019, 10, 31),-14)).to.equal("11/10/2019");
+    });  
+    test("Test pasado", function() {
+        expect(calcWorkingDate(new Date(2019, 10, 31),-1000)).to.equal("31/12/2015");
+    }); 
 });
 
 suite("Testing getDays", function() {
     test("Test pasado", function() {
-        expect(getWorkingDays(new Date(2019, 10, 13),new Date(2019, 10, 31))).to.equal(14);
+        expect(getWorkingDays(new Date(2019, 9, 13),new Date(2019, 10, 31))).to.equal(14);
     });
     test("Test pasado", function() {
-        expect(getWorkingDays(new Date(2019, 28, 31),new Date(2019, 10, 13))).to.equal(-14);
+        expect(getWorkingDays(new Date(2019, 9, 31),new Date(2019, 9, 13))).to.equal(-14);
     });
 });
