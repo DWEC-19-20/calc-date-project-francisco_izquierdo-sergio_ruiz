57a495bc3ea2e0e022be6a3bbb28661ef1b7a2ba
SergioRuizMasero <55686099+SergioRuizMasero@users.noreply.github.com>
2019-10-25 10:43:17 +0200

    Add files via upload

--- js/dateCalc.js
+++ js/dateCalc.js
@@ -1,4 +1,4 @@
-
+const ms = 24 * 60 * 60 * 1000;
 
 /* Función que suma o resta un número de dias naturales según el valor de operation 
    startdate: objeto Fecha 
@@ -6,7 +6,12 @@
    return el resultado como un string en formato dd/mm/YYYY
 */
 function calcDate(startdate, days) { 
-  return new Date().toLocaleDateString("es-ES");
+    
+    startdate.setMonth(startdate.getMonth() - 1);
+    startdate.setDate(startdate.getDate() + days);
+    
+    return startdate.toLocaleDateString('es-ES');
+    
 }
 
 /* Función que recibe dos fechas de tipo Date y devuelva el el número de días naturales que hay entre
@@ -16,7 +21,17 @@ function calcDate(startdate, days) {
   return número de días naturales entre las dos fechas
 */
 function getDays(startdate, endDate) {
-   return 0;
+     
+    startdate.setMonth(startdate.getMonth() - 1);
+    endDate.setMonth(endDate.getMonth() - 1);
+    
+    var diaI = startdate.getTime();
+    var diaF = endDate.getTime();
+    var dif = diaF - diaI;
+    var FechaF = parseInt((dif/ms).toFixed(0));
+    
+    return FechaF;
+    
 }
 
 /* Función que suma o resta un número de dias hábiles según el valor de operation 
@@ -24,8 +39,58 @@ function getDays(startdate, endDate) {
    days: número de días hábiles
    return el resultado como un string en formato dd/mm/YYYY
 */
-function calcWorkingDate(startdate, days) { 
-  return new Date().toLocaleDateString("es-ES");
+function calcWorkingDate(startdate, days) {     
+    
+    startdate.setMonth(startdate.getMonth() - 1);
+    
+    var festivos = [[1, 6],[],[],[],[1],[],[],[15],[],[12],[1],[6, 8, 25]];
+    
+    if(days > 0){
+        
+        for(let c = 0 ; c < days ; c++){
+        
+            startdate.setDate(startdate.getDate() + 1);
+        
+            for(let i = 0 ; i < festivos.length ; i++){
+                
+                for(let j = 0 ; j < festivos[i].length; j++){
+                    
+                    if(startdate.getMonth() == i && startdate.getDate() == festivos[i][j]) c--;
+                    
+                }
+                //Los test fallan porque los días festivos no los cuenta, cambiar eso.
+            }
+            
+            if(startdate.getDay() == 0 || startdate.getDay() == 6) c--;
+          
+        }
+        
+    }
+    
+    else if(days < 0){
+        
+        for(let c = days ; c <= 0 ; c++){
+        
+            startdate.setDate(startdate.getDate() - 1);
+        
+            for(let i = 0 ; i < festivos.length ; i++){
+                
+                for(let j = 0 ; j < festivos[i].length; j++){
+                    
+                    if(startdate.getMonth() == i && startdate.getDate() == festivos[i][j]) c--;
+                    
+                }
+                
+            }
+            
+            if(startdate.getDay() == 0 || startdate.getDay() == 6) c--;
+          
+        }
+        
+    }
+    
+    return startdate.toLocaleDateString('es-ES');
+    
 }
 
 /* Función que recibe dos fechas de tipo Date y devuelva el el número de días hábiles que hay entre
@@ -34,6 +99,6 @@ function calcWorkingDate(startdate, days) {
   endDate: objeto Fecha inicio
   return número de días hábiles entre las dos fechas*/
 function getWorkingDays(startdate, endDate) {
-   return 0;
+   return getDays(startdate, endDate);
 }
 
